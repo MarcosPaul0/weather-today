50988ed4531cdaf7a66cc8a76fb62c5425227dab
Marcos <marcosphip7@gmail.com>
2021-09-19 21:13:43 -0300

    correção de entrada

--- backend/src/modules/useCases/getCityWeather/GetCityWeatherUseCase.ts
+++ backend/src/modules/useCases/getCityWeather/GetCityWeatherUseCase.ts
@@ -16,7 +16,7 @@ export class GetCityWeatherUseCase {
   }
 
   async execute(city: string) {
-    const cityFormated = city.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    const cityFormated = city.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
 
     await this.registerSearch(cityFormated.join(' '))
   }
